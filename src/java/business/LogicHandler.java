055b0117f187d05aa814c4a58fb19174b989ba69
christiansahlstrom93 <>
2015-12-07 17:31:48 +0100

    test igen

--- src/java/business/LogicHandler.java
+++ src/java/business/LogicHandler.java
@@ -10,5 +10,5 @@ package business;
  * @author Christian
  */
 public class LogicHandler {
-    //här ska logik vara hehe nästa message....
+    //här ska logik vara hehe nästa message.... rövhål
 }
