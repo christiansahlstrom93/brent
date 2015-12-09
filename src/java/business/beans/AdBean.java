97e9054e994dd537fb1e8d2d519238d9c47df890
Christian <christian926@homtail.com>
2015-12-09 16:20:50 +0100

    borjar bli trott på maiuritz skit..

--- src/java/business/beans/AdBean.java
+++ src/java/business/beans/AdBean.java
@@ -1,5 +1,6 @@
 package business.beans;
 
+import business.beans.javahelpers.LoginCheck;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import json.CustomJsonParser;
-import org.json.JSONObject;
 
 /**
  *
@@ -27,50 +26,24 @@ public class LoginServlet extends HttpServlet {
     @EJB
     private AdBeanLocal adBean;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
- 
+
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-        
-         try {
+
+        try {
             CustomJsonParser jsonParser = new CustomJsonParser("username", request);
             PrintWriter out = response.getWriter();
             String username = jsonParser.getString();
@@ -81,16 +54,18 @@ public class LoginServlet extends HttpServlet {
             System.out.println("password " + password + "");
             System.out.println("************");
 
-          String result = adBean.loginCheck(username, password);
-             System.out.println(result + "detta är servlets result");
-            out.print("kug");
-            
+            String result = adBean.loginCheck(username, password);
+            out.print(result);
+
+
+            /*JSONObject mainObj = new JSONObject();
+            mainObj.put("usr", adBean.loginCheck(username, password));
+            out.print(mainObj);*/
         } catch (Exception e) {
-             System.out.println("servlet error"+ e);
+            System.out.println("servlet error" + e);
         }
     }
 
-    
     @Override
     public String getServletInfo() {
         return "Short description";
